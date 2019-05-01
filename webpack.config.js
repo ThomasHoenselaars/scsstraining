24063a9c90c7664448c4d7c23a0dd42b0a1645a6
Thomas Hoenselaars <heemsteda@gmail.com>
2019-05-01 12:17:20 +0200

    margin classes & register

--- webpack.config.js
+++ webpack.config.js
@@ -175,6 +175,15 @@ module.exports = (env = {}, argv) => {
             removeStyleLinkTypeAttributes: true
           }
         }),
+        new plugins.html({
+          template: "register.html",
+          filename: "register.html",
+          minify: {
+            removeScriptTypeAttributes: true,
+            removeStyleLinkTypeAttributes: true
+          }
+        }),
+
         new plugins.progress({
           color: "#5C95EE"
         })
